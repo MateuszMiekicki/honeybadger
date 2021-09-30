fd3a1787cd589e6e48ad988cf21f83edd289d5bb
Mateusz Miekicki <miekickimateusz@gmail.com>
2021-09-30 22:10:09 +0200

    Initial configuration of cmake and submodules.
    Added gtest and doxygen.

---
+++ cmake/doxygen.cmake
@@ -0,0 +1,12 @@
+find_package(Doxygen)
+if (DOXYGEN_FOUND)
+    set(DOXYGEN_CONFIFGURE ${CMAKE_CURRENT_SOURCE_DIR}/doc/Doxyfile)
+    set(DOXYGEN_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/doc)
+    doxygen_add_docs(Doxygen
+        ALL
+        ${DOXYGEN_CONFIFGURE}
+        COMMENT "Generation of documentation for versions ${CMAKE_PROJECT_VERSION}."
+    )
+else ()
+    message("Doxygen need to be installed to generate the doxygen documentation")
+endif ()
