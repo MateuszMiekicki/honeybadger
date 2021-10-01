c234ff1c53a45083cea4fd10aab08805aa672978
Mateusz Miekicki <miekickimateusz@gmail.com>
2021-10-01 20:44:04 +0200

    Performance testing framework added.
    Added cmake for google benchmark.

---
+++ benchmark/cmake/googlebenchmark.cmake
@@ -0,0 +1,5 @@
+FetchContent_Declare(googlebenchmark
+  GIT_REPOSITORY https://github.com/google/benchmark.git
+  GIT_TAG        v1.6.0
+)
+FetchContent_MakeAvailable(googlebenchmark)
--- test/cmake/googletest.cmake
+++ test/cmake/googletest.cmake
@@ -1,5 +1,4 @@
-FetchContent_Declare(
-  googletest
+FetchContent_Declare(googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG        release-1.11.0
 )
