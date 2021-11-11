61692b7ac5ff1bf882903ae61fb4fbcd3b3e7108
Mateusz Miękicki <miekickimateusz@gmail.com>
2021-11-11 22:59:15 +0100

    Added an example of how to convert images to a given color set.

---
+++ example/image/preprocessing/colorConvert.cc
@@ -0,0 +1,17 @@
+#include <honeybadger/image/preprocessing/Color.hh>
+#include <opencv2/opencv.hpp>
+
+int main()
+{
+    using namespace honeybadger::image::preprocessing;
+    {
+        std::string jpg = "img.jpg";
+        cv::Mat imgjpg = cv::imread(jpg);
+        const auto grayscale = Color::convertToGrayscale(imgjpg);
+    }
+    {
+        std::string png = "img.png";
+        cv::Mat imgpng = cv::imread(png);
+        const auto binarization = Color::binarization(imgpng);
+    }
+}
