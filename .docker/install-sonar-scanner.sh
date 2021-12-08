edcabc0cb674d57be2de9f096ec73605a53994a5
Mateusz Miękicki <miekickimateusz@gmail.com>
2021-12-08 21:51:41 +0100

    Modified the image building script for CI and the script itself for CircleCI.
    Dodano sonar-scanner dla dockera.

---
+++ .docker/install-sonar-scanner.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eux
+
+SONAR_SCANNER_VERSION=4.6.2.2472
+
+# Only install java if not already present (to avoid conflicting Java version)
+arg_java=""
+if ! which java >/dev/null; then
+  arg_java=openjdk11-jre
+fi
+
+apk add --no-cache \
+  ca-certificates \
+  $arg_java
+
+mkdir -p /opt
+
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}.zip -O /opt/sonar-scanner.zip
+unzip /opt/sonar-scanner.zip -d /opt
+rm /opt/sonar-scanner.zip
+ln -s /opt/sonar-scanner-${SONAR_SCANNER_VERSION}/bin/sonar-scanner /usr/bin/sonar-scanner
