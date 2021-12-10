89a8c5f6dfa0c6ddd30cc1c0556031eebeea9517
Mateusz Miękicki <miekickimateusz@gmail.com>
2021-12-10 22:01:56 +0100

    Changing the base image from Alpine to ubuntu for CI for docker.
    The reason is that the sonar-scanner does not work on the Alpine image.

--- .docker/install-sonar-scanner.sh
+++ .docker/install-sonar-scanner.sh
@@ -6,15 +6,12 @@ SONAR_SCANNER_VERSION=4.6.2.2472
 # Only install java if not already present (to avoid conflicting Java version)
 arg_java=""
 if ! which java >/dev/null; then
-  arg_java=openjdk11-jre
+  arg_java=default-jre
 fi
 
-apk add --no-cache \
-  ca-certificates \
-  $arg_java
+apt-get install -y $arg_java
 
 mkdir -p /opt
-
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_SCANNER_VERSION}.zip -O /opt/sonar-scanner.zip
 unzip /opt/sonar-scanner.zip -d /opt
 rm /opt/sonar-scanner.zip
