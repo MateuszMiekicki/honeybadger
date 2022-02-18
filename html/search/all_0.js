b4aa5302e9ad09f91637fac7743be2a55f0c2d1a
Mateusz Miękicki <miekickimateusz@gmail.com>
2022-02-18 10:25:00 +0100

    Manually adding the documentation folder on a trial basis.

---
+++ html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['color_0',['Color',['../classhoneybadger_1_1image_1_1preprocessing_1_1_color.html',1,'honeybadger::image::preprocessing']]],
+  ['converter_1',['Converter',['../classhoneybadger_1_1convert_1_1_converter.html',1,'honeybadger::convert']]],
+  ['converttograyscale_2',['convertToGrayscale',['../classhoneybadger_1_1image_1_1preprocessing_1_1_color.html#a7fe9c33718cc1fa45d8308a0530c9cef',1,'honeybadger::image::preprocessing::Color']]]
+];
---
+++ html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['color_0',['Color',['../classhoneybadger_1_1image_1_1preprocessing_1_1_color.html',1,'honeybadger::image::preprocessing']]],
+  ['converter_1',['Converter',['../classhoneybadger_1_1convert_1_1_converter.html',1,'honeybadger::convert']]]
+];
