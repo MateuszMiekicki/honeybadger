06af7eb787d4e6f01e600d4d771cbf4a83162d9b
ci-build <>
2022-02-18 10:38:40 +0000

    as

--- html/search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['converttograyscale_0',['convertToGrayscale',['../classhoneybadger_1_1image_1_1preprocessing_1_1_color.html#a7fe9c33718cc1fa45d8308a0530c9cef',1,'honeybadger::image::preprocessing::Color']]]
-];
--- html/search/functions_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['thresholding_0',['thresholding',['../classhoneybadger_1_1image_1_1preprocessing_1_1_color.html#ac55f094ec165fd079ecfc012c3297cf2',1,'honeybadger::image::preprocessing::Color']]]
-];
