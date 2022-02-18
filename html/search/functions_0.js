6bd5c64e8efb7e45f80a834f44964ef5ac640756
ci-build <>
2022-02-18 11:41:47 +0000

    95

---
+++ html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['converttograyscale_6',['convertToGrayscale',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html#a7fe9c33718cc1fa45d8308a0530c9cef',1,'honeybadger::image::preprocessing::Color']]]
+];
---
+++ html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['thresholding_7',['thresholding',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html#ac55f094ec165fd079ecfc012c3297cf2',1,'honeybadger::image::preprocessing::Color']]]
+];
