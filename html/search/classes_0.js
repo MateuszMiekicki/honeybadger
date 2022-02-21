59da83849a4e25cd340fa6aafe68b04006f297ea
ci-build <>
2022-02-21 19:13:28 +0000

    auto commit [ci skip]118

---
+++ html/search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['color_4',['Color',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html',1,'honeybadger::image::preprocessing']]],
+  ['converter_5',['Converter',['../classhoneybadger_1_1converter_1_1Converter.html',1,'honeybadger::converter']]]
+];
