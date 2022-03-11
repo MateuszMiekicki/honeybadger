51017fd49abb585f482e57dbd2120d97df384869
ci-build <>
2022-03-11 09:09:58 +0000

    auto commit [ci skip]134

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['converttograyscale_6',['convertToGrayscale',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html#a7fe9c33718cc1fa45d8308a0530c9cef',1,'honeybadger::image::preprocessing::Color']]]
+  ['convert_7',['convert',['../classhoneybadger_1_1converter_1_1Converter.html#a5821d4912bb4a60d8194cbe5ce5d323a',1,'honeybadger::converter::Converter']]],
+  ['converttograyscale_8',['convertToGrayscale',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html#a7fe9c33718cc1fa45d8308a0530c9cef',1,'honeybadger::image::preprocessing::Color']]]
 ];
