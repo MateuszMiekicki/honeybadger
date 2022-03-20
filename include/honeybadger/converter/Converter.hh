8a4f41b2823790ee3f1910298c558a67beba7371
Mateusz Miekicki <miekickimateusz@gmail.com>
2022-03-21 00:04:42 +0100

    [ci skip] Added enum for typing file for converting

--- include/honeybadger/converter/Converter.hh
+++ include/honeybadger/converter/Converter.hh
@@ -5,14 +5,22 @@
 
 namespace honeybadger::converter
 {
+enum class Format
+{
+    Undefined = 0,
+    PDF,
+    PNG
+};
+
 /// @brief An interface for classes that will be responsible for converting
 /// one type of file into another. By default, these are image/pdf/text files.
 class Converter
 {
   public:
     /// @brief Main method used to convert between type.
+    /// @param convertTo What you want to convert the PDF to.
     /// @return Array of byte with converted type.
-    virtual std::vector<std::byte> convert() = 0;
+    virtual std::vector<std::byte> convert(const Format convertTo) = 0;
     virtual ~Converter() = default;
 };
 } // namespace honeybadger::converter
