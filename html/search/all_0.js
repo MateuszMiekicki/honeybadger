f5e1731beb4ee3eae2b2ca70076979365afe4262
ci-build <>
2022-04-12 22:18:06 +0000

    auto commit [ci skip]142

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['color_0',['Color',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html',1,'honeybadger::image::preprocessing']]],
-  ['convert_1',['convert',['../classhoneybadger_1_1converter_1_1Converter.html#a5821d4912bb4a60d8194cbe5ce5d323a',1,'honeybadger::converter::Converter']]],
+  ['convert_1',['convert',['../classhoneybadger_1_1converter_1_1Converter.html#aa85fd7e3a2bc215392f272c5e4ddf1fa',1,'honeybadger::converter::Converter']]],
   ['converter_2',['Converter',['../classhoneybadger_1_1converter_1_1Converter.html',1,'honeybadger::converter']]],
   ['converttograyscale_3',['convertToGrayscale',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html#a7fe9c33718cc1fa45d8308a0530c9cef',1,'honeybadger::image::preprocessing::Color']]]
 ];
--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['convert_7',['convert',['../classhoneybadger_1_1converter_1_1Converter.html#a5821d4912bb4a60d8194cbe5ce5d323a',1,'honeybadger::converter::Converter']]],
+  ['convert_7',['convert',['../classhoneybadger_1_1converter_1_1Converter.html#aa85fd7e3a2bc215392f272c5e4ddf1fa',1,'honeybadger::converter::Converter']]],
   ['converttograyscale_8',['convertToGrayscale',['../classhoneybadger_1_1image_1_1preprocessing_1_1Color.html#a7fe9c33718cc1fa45d8308a0530c9cef',1,'honeybadger::image::preprocessing::Color']]]
 ];
